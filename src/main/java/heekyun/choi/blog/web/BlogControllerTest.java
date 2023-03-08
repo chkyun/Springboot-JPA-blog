38190f9085c322db487b7be7dd4d6ecf96af4ff9
chkyun <chkyun@naver.com>
2023-03-08 18:32:27 +0900

    환경세팅 완료

---
+++ src/main/java/heekyun/choi/blog/web/BlogControllerTest.java
@@ -0,0 +1,13 @@
+package heekyun.choi.blog.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogControllerTest {
+	
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "hello spring boot";
+	}
+}
---
+++ src/test/java/heekyun/choi/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package heekyun.choi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
